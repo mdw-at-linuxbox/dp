7818c605b44ec3859147dface377a7e7c1f37152
Marcus Watts <mwatts@redhat.com>
2019-01-06 22:45:09 -0500

    common low-core locations

---
+++ psa.s
@@ -0,0 +1,21 @@
+psa dsect
+strtpsw ds d
+initccw1 ds d
+initccw2 ds d
+exopsw ds d
+svopsw ds d
+pgopsw ds d
+mcopssw ds d
+ioopsw ds d
+ org psa+64
+thecsw ds d
+thecaw ds f
+ ds f
+timer ds f
+ ds f
+exnpsw ds d
+svnpsw ds d
+pgnpsw ds d
+mcnpssw ds d
+ionpsw ds d
+scanout ds xl256
